99086c4b20d5ecc7fb3d6edeeb8442b4e32ceb42
Mike McMahon <mmm219@mcc.northwestern.edu>
2019-05-17 21:24:50 -0500

    before leaving starbucks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,42 @@
 $(document).ready(function () {
 
     // initialize variables
-    var wins = "0";
+    var wins = 0;
     console.log(wins);
-    var losses = "0";
+    var losses = 0;
     console.log(losses);
-    var targetNumber = "0";
+    var targetNumber = 0;
+    userScore = 0;
+    console.log(userScore);
 
     // create arrays for random number generation and image array
     var jewelValue = [Math.floor(Math.random() * 11) + 1, Math.floor(Math.random() * 11) + 1, Math.floor(Math.random() * 11) + 1, Math.floor(Math.random() * 11) + 1];
     console.log(jewelValue);
     var imageArray = ["./assets/images/jewel1.jpg", "./assets/images/jewel2.jpg", "./assets/images/jewel3.jpg", "./assets/images/jewel4.jpg"]
     console.log(imageArray);
+ 
+    // calculates random computer guess
+    function calculateTarget() {
+        targetNumber = Math.floor(Math.random() * 101) + 19;
+        // show this on the page
+        $("#cpuGuess").html("<h1>" + targetNumber + "</h1>"); 
+    }
+    // run the above function
+    calculateTarget();
 
-
-
-
-
-
+    // tabulates wins
+    function winCount () {
+        if (userScore === targetNumber) {
+        wins++;
+        alert("You win! Congrats!");
+        // show this on the page
+        $("#wins").html("<h3>" + "Wins:" + "<br>" + wins + "</h3>");
+        } else {
+            wins = "";
+        } 
+    }
+    // run the above function
+    winCount();
 
 
 
