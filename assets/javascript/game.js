96ca917514ff828900358da9c0bfc0e3f827d2a3
Mike McMahon <mmm219@mcc.northwestern.edu>
2019-05-17 20:40:56 -0500

    button added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // initialize variables
-
-    var targetNumber = [Math.floor(Math.random()*11)+1, Math.floor(Math.random()*11)+1, Math.floor(Math.random()*11)+1, Math.floor(Math.random()*11)+1];
     var wins = "0";
+    console.log(wins);
     var losses = "0";
+    console.log(losses);
+    var targetNumber = "0";
 
-    // Next we create a for loop to create crystals for every numberOption.
-  for (var i = 0; i < numberOptions.length; i++) {
+    // create arrays for random number generation and image array
+    var jewelValue = [Math.floor(Math.random() * 11) + 1, Math.floor(Math.random() * 11) + 1, Math.floor(Math.random() * 11) + 1, Math.floor(Math.random() * 11) + 1];
+    console.log(jewelValue);
+    var imageArray = ["./assets/images/jewel1.jpg", "./assets/images/jewel2.jpg", "./assets/images/jewel3.jpg", "./assets/images/jewel4.jpg"]
+    console.log(imageArray);
 
-    // For each iteration, we will create an imageCrystal
-    var imageCrystal = $("<img>");
-
-    // First each crystal will be given the class ".crystal-image".
-    // This will allow the CSS to take effect.
-    imageCrystal.addClass("crystal-image");
-
-    // Each imageCrystal will be given a src link to the crystal image
-    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-
-    // Each imageCrystal will be given a data attribute called data-crystalValue.
-    // This data attribute will be set equal to the array value.
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-    $("#crystals").append(imageCrystal);
-  }
 
 
 
