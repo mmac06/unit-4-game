877c8b16d5b4589c6942f87dabf2851b1b189a8a
Mike McMahon <mmm219@mcc.northwestern.edu>
2019-05-17 20:10:26 -0500

    added random number generator

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
 
+    // initialize variables
+
+    var targetNumber = [Math.floor(Math.random()*11)+1, Math.floor(Math.random()*11)+1, Math.floor(Math.random()*11)+1, Math.floor(Math.random()*11)+1];
+    var wins = "0";
+    var losses = "0";
+
+    // Next we create a for loop to create crystals for every numberOption.
+  for (var i = 0; i < numberOptions.length; i++) {
+
+    // For each iteration, we will create an imageCrystal
+    var imageCrystal = $("<img>");
+
+    // First each crystal will be given the class ".crystal-image".
+    // This will allow the CSS to take effect.
+    imageCrystal.addClass("crystal-image");
+
+    // Each imageCrystal will be given a src link to the crystal image
+    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+
+    // Each imageCrystal will be given a data attribute called data-crystalValue.
+    // This data attribute will be set equal to the array value.
+    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+    $("#crystals").append(imageCrystal);
+  }
+
+
 
 
 
